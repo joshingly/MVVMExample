7da5c65878dbdb22aa005fbf24d3f5e1e6ce4438
Joshua Antonishen <joshua@antonishen.com>
2022-06-02 07:45:10 -0500

    Initial commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Joshua Antonishen on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
