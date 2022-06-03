3e4b507ead037c0c7afb2dc70703a92544672fe6
Joshua Antonishen <joshua@antonishen.com>
2022-06-03 09:10:50 -0500

    Outlets & actions

--- MVVMExample/Screens/ContactForm/ContactFormViewController.swift
+++ MVVMExample/Screens/ContactForm/ContactFormViewController.swift
@@ -9,11 +9,23 @@ import Foundation
 import UIKit
 
 class ContactFormViewController: UIViewController {
+  @IBOutlet var previousButton: UIButton!
+  @IBOutlet var nextButton: UIButton!
+  @IBOutlet var cancelButton: UIButton! { didSet {
+    cancelButton.addTarget(self, action: #selector(pressCancel), for: .touchUpInside)
+  }}
+  @IBOutlet var saveButton: UIButton!
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     setup()
   }
+
+  @objc func pressPrevious() {}
+  @objc func pressNext() {}
+  @objc func pressCancel() { self.dismiss(animated: true) }
+  @objc func pressSave() {}
 
   func setup() {
     view.backgroundColor = .lightGray
