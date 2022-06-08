2a458084f6b02f8942bc9fcf75fa6dec64a09e98
Joshua Antonishen <joshua@antonishen.com>
2022-06-08 12:30:08 -0500

    Add contact fields

--- MVVMExample/Models/Contact.swift
+++ MVVMExample/Models/Contact.swift
@@ -7,4 +7,24 @@
 
 import Foundation
 
-class Contact {}
+class Contact {
+  var name: String
+  var location: String
+  var age: Int?
+  var birthday: Date?
+  var kind: String
+
+  init(
+    name: String = "",
+    location: String = "",
+    age: Int? = nil,
+    birthday: Date? = nil,
+    kind: String = ""
+  ) {
+    self.name = name
+    self.location = location
+    self.age = age
+    self.birthday = birthday
+    self.kind = kind
+  }
+}
